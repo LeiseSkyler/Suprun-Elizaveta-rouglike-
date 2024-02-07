66d6b2237156f08c77514ca5913ab83a3c885b4f
LeiseSkyler <133450148+LeiseSkyler@users.noreply.github.com>
2024-02-07 22:40:51 +0900

    new classes
    Добавлены векторы и несколько новых классов

---
+++ Game_Entity.cs
@@ -0,0 +1,42 @@
+﻿namespace rogalik__2
+{
+    partial class Program
+    {
+        public class Game_Entity : Game_Object
+        {
+            private int hp;
+            public int HP
+            {
+                get { return hp; }
+                set
+                {
+                    hp = value;
+                    if (hp <= 0)
+                    {
+                        Destroy();
+                    }
+                }
+            }
+            
+            public Game_Entity(int hp, Vector2 position, char symbol, bool passable = false) 
+                : base(position, symbol, passable)
+            {
+                HP = hp;
+            }
+            
+            public void Get_Damage(int damage)
+            {
+                HP -= damage;
+            }
+
+            public override void Update()
+            {
+                base.Update();
+            }
+        } 
+
+    }
+
+
+}
+
---
+++ Game_Object.cs
@@ -0,0 +1,58 @@
+﻿namespace rogalik__2
+{
+    partial class Program
+    {
+        public class Game_Object
+        {
+            public Vector2 Position 
+            { 
+                get
+                {
+                    return _position;
+                } 
+              
+                set
+                {
+                    Previous_Position = _position;
+                    _position = value;
+                } 
+            }
+            public Game_Object(Vector2 position, char symbol, bool passable)
+            {
+                Position = position;
+                Symbol = symbol;
+                Passable = passable;
+                Previous_Position = new Vector2(_position.X, _position.Y);
+            }
+
+            public Vector2 _position;
+            public Vector2 Previous_Position { get; set; }
+            public char Symbol { get; set; }
+            public bool Passable { get; set; }
+
+            public virtual void Update()
+            {
+
+            }
+
+            public event Action<Game_Object> On_Destroy;
+            public event Action<Game_Object, Vector2> Move;
+
+            public virtual void Destroy_Action()
+            {
+               
+            }
+
+            public void Destroy()
+            {
+                Destroy_Action();
+                if (On_Destroy != null) On_Destroy(this);
+
+            }
+        }
+
+    }
+
+
+}
+
